d2743265db07a05030da9cf55b7985423c4b6332
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2023-03-14 19:12:01 -0500

    Add files via upload

---
+++ Javascript Fetch APIs/main.js
@@ -0,0 +1,54 @@
+const apiURL = "https://jsonplaceholder.typicode.com/users";
+let data;
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((data) => manipulateUserInfo(data))
+  .catch((err) => console.log(err));
+
+function manipulateUserInfo(data) {
+  console.log(data);
+  const userTable = document.createElement("div");
+  userTable.setAttribute("class", "project-table-wrapper");
+
+  let rows = "";
+  let tableHeadings = "";
+
+  for (let key in data[0]) {
+    tableHeadings += `<th>${key.charAt(0).toUpperCase() + key.slice(1)}</th>`;
+    console.log(key);
+  }
+  for (let i = 0; i < data.length; i++) {
+    rows += `
+      <tr>
+        <td>${data[i].id}</td>
+        <td>${data[i].name}</td>
+        <td>${data[i].username}</td>
+        <td>${data[i].email}</td>
+        <td>${
+          (data[i].address.street,
+          data[i].address.suite,
+          data[i].address.city.toUpperCase(),
+          data[i].address.zipcode)
+        }</td>
+        <td>${data[i].phone}</td>
+        <td>${data[i].website}</td>
+        <td><span style="font-weight: 700">${data[i].company.name}:</span> ${
+      (data[i].company.catchPhrase, data[i].company.bs)
+    }</td>
+      </tr>
+    `;
+  }
+  userTable.innerHTML = `
+            <table class="project-table table">
+                <thead>
+                <tr>
+                ${tableHeadings}
+                </tr>
+                </thead>
+                <tbody>
+                        ${rows}
+                </tbody>
+            </table>
+        `;
+  document.body.insertAdjacentElement("afterbegin", userTable);
+}
